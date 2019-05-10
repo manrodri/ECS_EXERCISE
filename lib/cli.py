f311290a9b569d1de6076a3732fa65772e70d2f1
manuel <manuel@server1.example.com>
2019-05-10 11:40:55 +0100

    add args checking

--- lib/cli.py
+++ lib/cli.py
@@ -1,4 +1,14 @@
 import sys
 
-name = sys.argv[1]
-print 'hello {}'.format(name)
+# checking arguments
+num_of_args = len(sys.argv) -1
+if num_of_args != 6:
+    print 'Error: num of args must be 6'
+    print 'see help'
+
+
+def main():
+ pass
+
+if __name__ == '__main__':
+    main()
